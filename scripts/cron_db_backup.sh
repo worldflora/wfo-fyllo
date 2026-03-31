e4507cf0afb9e568e34afa6a81ac9622b8e743e2
Roger <rhyam@rbge.org.uk>
2026-03-31 10:08:34 +0100

    backup new db name

--- scripts/cron_db_backup.sh
+++ scripts/cron_db_backup.sh
@@ -1,12 +1,12 @@
 # will backup the live database as specified in the config file.
 # suitable for being run by cron
 today=$(date +"%Y-%m-%d-%H-%M-%S")
-filename="../data/db_dumps/wfo_facets_${today}.sql"
-mysqldump wfo_facets > $filename
+filename="../data/db_dumps/fyllo_${today}.sql"
+mysqldump fyllo > $filename
 echo $filename
 gzip $filename
 
 # prevent backups filling disk
-find ../data/db_dumps -type f -mtime +30 -delete
+find ../data/db_dumps -type f -mtime +10 -delete
 
 
