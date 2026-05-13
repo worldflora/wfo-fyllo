75dec05fb072ad6693e253bc806d4860faf8a89a
Roger <rhyam@rbge.org.uk>
2026-05-13 23:27:03 +0100

    add decimal part

--- www/api.php
+++ www/api.php
@@ -515,13 +515,19 @@ function return_scores_metadata(){
         $solr_docs = array();
 
         // if they haven't set a since the we begin at the start of the epoch
-        if(!isset($_GET['since'])) $modified_stamp = 0;
-        else $modified_stamp = floor((double)$_GET['since']);
+        if(!isset($_GET['since'])){
+            $modified_stamp = 0;
+            $id = 0;
+        }else{
+            list($modified_stamp, $id) = explode('.', $_GET['since']); 
+        } 
+
+
 
         $sql = "SELECT 
             `wfo_scores`.*, concat(UNIX_TIMESTAMP(`modified`), '.', id) as last_modified_d  
             FROM `wfo_scores` 
-            WHERE `modified` > FROM_UNIXTIME($modified_stamp)
+            WHERE (`modified` > FROM_UNIXTIME($modified_stamp) OR (`modified` > FROM_UNIXTIME($modified_stamp) AND id > $id ))
             AND meta_json is not null 
             ORDER BY modified, id
             LIMIT 1000;"; 
